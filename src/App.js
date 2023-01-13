1b1e120baae45410e305900fee688933b536dc2b
Ihor Domnich <mr.igordomnich@gmail.com>
2023-01-13 20:36:09 +0100

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -38,12 +38,23 @@ function App() {
     })));
   };
 
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ]);
+  };
+
   return (
     <Container>
       <Header title="Tasks list" />
       <Section
         title="Add new task"
-        body={<Form />}
+        body={<Form addNewTask={addNewTask} />}
       />
       <Section
         title="Task lisk"
