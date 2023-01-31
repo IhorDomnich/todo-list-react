7fc58a4ac8be7f1a10a6104daae80694fa976396
Ihor Domnich <mr.igordomnich@gmail.com>
2023-02-01 00:06:00 +0100

    application split into features

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from "./GlobalStyle";
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { theme } from "./theme";
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
