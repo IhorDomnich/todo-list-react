f1ecd5851f4789654ed278f00ecf4f5cf0e95d65
Ihor Domnich <mr.igordomnich@gmail.com>
2023-02-10 21:48:35 +0100

    added button

---
+++ src/features/tasks/tasksPage/Buttons/index.js
@@ -0,0 +1,39 @@
+import { useSelector, useDispatch } from "react-redux";
+import { Wrapper } from "./styled";
+import Button from "../Button";
+import {
+    toggleHideDone,
+    setAllDone,
+    selectAreTasksEmpty,
+    selectIsEveryTaskDone,
+    selectHideDone,
+} from "../../tasksSlice";
+
+const Buttons = () => {
+    const areTasksEmpty = useSelector(selectAreTasksEmpty);
+    const isEveryTaskDone = useSelector(selectIsEveryTaskDone);
+    const hideDone = useSelector(selectHideDone);
+
+    const dispatch = useDispatch();
+
+    return (
+        <Wrapper>
+            {!areTasksEmpty && (
+                <>
+                    <Button onClick={() => dispatch(toggleHideDone())}>
+                        {hideDone ? "Show " : "Hide "}
+                        completed
+                    </Button>
+                    <Button
+                        onClick={() => dispatch(setAllDone())}
+                        disabled={isEveryTaskDone}
+                    >
+                        Complete all
+                    </Button>
+                </>
+            )}
+        </Wrapper>
+    )
+};
+
+export default Buttons;
