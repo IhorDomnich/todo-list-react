34f365f258525c8f8d192fa06e50aa00d1f73900
Ihor Domnich <mr.igordomnich@gmail.com>
2023-02-10 21:39:37 +0100

    correction text

--- src/App.js
+++ src/App.js
@@ -1,3 +1,28 @@
-import Tasks from "./features/tasks/Tasks";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import Author from "./features/author/AuthorPage";
+import Tasks from "./features/tasks/tasksPage";
+import Task from "./features/tasks/tasksPage/TaskPage";
+import {toAuthor, toTasks, toTask} from "./routes";
+import Navigation from "./Navigatoin";
 
-export default () => <Tasks />;
+
+export default () => (
+    <HashRouter>
+       <Navigation />
+
+            <Switch>
+                <Route path={toTask()}>
+                    <Task />
+                </Route>
+                <Route path={toTasks()}>
+                    <Tasks />
+                </Route>
+                <Route path={toAuthor()}>
+                    <Author />
+                </Route>
+                <Route>
+                    <Redirect to={toTasks()} />
+                </Route>
+            </Switch>
+    </HashRouter>
+);
