c1809c1be62a4e9b8864ecfbb345be6ba3a510e8
Ihor Domnich <mr.igordomnich@gmail.com>
2023-02-10 21:41:22 +0100

    correction color and max-width

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.main`
-    max-width: 900px;
+    max-width: 1200px;
     padding: 20px;
     margin: 0 auto;
-    background-color: ${({ theme }) => theme.color.beige};
+    background-color: ${({ theme }) => theme.color.gallery}
 `;
